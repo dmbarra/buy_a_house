fa53a5a038af3c00cdc5dd77bcb0d38e0c92a96f
Daniel Barra <dmbarra@gmail.com>
2021-02-18 17:45:16 -0300

    Add regex to extract just domains

--- buy_a_house/spiders/rent_property.py
+++ buy_a_house/spiders/rent_property.py
@@ -1,6 +1,12 @@
-import scrapy
+import re
 
+import scrapy
+from scrapy.linkextractors import LinkExtractor
 from polls.models.basic_crawler import BasicCrawler
+
+allow_extract = [r'.com/', r'.com.br/']
+deny_domains_extract = ['google.com', 'web.whatsapp.com', 'bb.com.br']
+get_just_address = re.compile('(.*\.)?(htt.*)\.com.br|(.*\.)?(htt.*)\.com')
 
 
 def load_url():
@@ -10,10 +16,14 @@ def load_url():
     return urls
 
 
-
 class rentPropertySpider(scrapy.Spider):
   name = "rent_layer"
   start_urls = load_url()
 
   def parse(self, response):
-      print(response)
+      xlink = LinkExtractor(allow=allow_extract, deny_domains=deny_domains_extract)
+      for link in xlink.extract_links(response):
+          if len(str(link)) > 200:
+            matchResult = get_just_address.search(link.url)
+            if matchResult:
+                print("REFINED: "+matchResult.group(0))
