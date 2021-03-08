8109f76f845f54ca6b49a0bfd1a820fae6d6bbd7
Daniel Barra <dmbarra@gmail.com>
2021-03-08 14:54:19 -0300

    transform url as href

--- polls/tables/rental_property.py
+++ polls/tables/rental_property.py
@@ -1,9 +1,11 @@
-from django_tables2 import tables
+from django_tables2 import tables, URLColumn
 
 from polls.models.rental_property import RentalProperty
 
 
 class RentalPropertyTable(tables.Table):
+    url = name = URLColumn()
+
     class Meta:
         model = RentalProperty
         template_name = "django_tables2/bootstrap.html"
