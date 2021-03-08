4529145e60cc4d81b5ded43c77f9607515e1dceb
Daniel Barra <dmbarra@gmail.com>
2021-03-08 14:29:45 -0300

    Add selenium

--- buy_a_house/spiders/find_the_property.py
+++ buy_a_house/spiders/find_the_property.py
@@ -1,24 +1,32 @@
+import time
+
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from polls.models.scrapy_improviments import ScrapyImproviment
+from selenium.webdriver.chrome import webdriver
+
+from polls.models.rental_property import RentalProperty
 
 
 def load_url():
     urls = []
-    for e in ScrapyImproviment.objects.all():
-        urls.append(e.improved.replace("{city}", "Barbacena"))
+    for e in RentalProperty.objects.all():
+        urls.append('https://www.google.com.br/search?q=site:'+e.url+'+venda+barbacena/mg')
     return urls
 
 
 class FindThePropertySpider(scrapy.Spider):
   name = "property_layer"
   start_urls = load_url()
+  handle_httpstatus_list = [302,303,429]
+
+  def __init__(self):
+      self.driver = webdriver.WebDriver()
+      self.driver.maximize_window()
 
   def parse(self, response):
-      print(response.body)
-      xlink = LinkExtractor()
-      for link in xlink.extract_links(response):
-          if len(str(link)) > 200:
-              print('----------------------')
-              print(link)
-              print('******************')
+      for url in self.start_urls:
+          self.driver.get(url)
+          if(self.driver.find_element_by_id("recaptcha")):
+              self.driver.find_element_by_css_selector(".recaptcha-checkbox-border").click()
+          print(self.driver.title)
+          time.sleep(40)
+
